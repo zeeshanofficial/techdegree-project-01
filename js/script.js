4406626fa5c20387fe68759f4f45d5d931e83648
zeeshanofficial <34814964+zeeshanofficial@users.noreply.github.com>
2019-03-17 15:45:25 +0530

    Update script.js
    Refactored bunch of lines as per the direction from the slack group Huge props to @Adel AK

--- js/script.js
+++ js/script.js
@@ -43,7 +43,9 @@ function getRandomQuote(){
 // the random quote is called by calling the function and its value is stored in a variable
 function printQuote(){
   var randomNumber = getRandomQuote();
-// this variable is then used to concatinated the p tag by pointing to key value pairs of the array object.
+  var colour = () => Math.floor(Math.random() * 256);
+
+// the randomNumber variable is used to concatinated the p tag by pointing to key value pairs of the array object.
     var listHtml = '<p class = "quote">' + randomNumber.quote + '</p>';
     listHtml += '<p class = "source">' + randomNumber.source ; 
     if(randomNumber.year){
@@ -52,25 +54,22 @@ function printQuote(){
       listHtml += '<span class = "citation">' + randomNumber.citation + '</span>'
     }
   listHtml += '</p>';
-
 /*** listHtml is then sent to innerHTML of the quote-box div and is used to dynamically change the quotes randomly, 
  * or what we call as 'manipulating the DOM' .
 ***/
 
   document.getElementById("quote-box").innerHTML = listHtml;
-  
+  document.body.style.backgroundColor = 'rgb(' + colour() + ',' + colour() + ',' + colour() + ')'; 
+//referred from w3schools, link : https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp
 }
-
-function styleHtml(){
-    var colour = () => Math.floor(Math.random() * 256);
-    document.body.style.backgroundColor = 'rgb(' + colour() + ',' + colour() + ',' + colour() + ')'; 
-    //referred from w3schools, link : https://www.w3schools.com/jsref/prop_style_backgroundcolor.asp
-}
-
 
 function autoRefresh(){
-var auto = setInterval( styleHtml, 2000);
-auto += setInterval( printQuote, 2000);
+  var auto = setInterval( printQuote, 2000);
+}
+
+function master(){
+  var one = printQuote();
+  var two = autoRefresh();
 }
 //referred MDN docs but found it hard to grasp, got into W3schools this time again and done, link : https://www.w3schools.com/jsref/met_win_setinterval.asp
 
@@ -80,8 +79,7 @@ auto += setInterval( printQuote, 2000);
   function.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-document.getElementById('loadQuote').addEventListener("click", styleHtml, false);
-document.getElementById('loadQuote').addEventListener("click", autoRefresh, false);
+document.getElementById('loadQuote').addEventListener("click", master, false);
+
 
 
